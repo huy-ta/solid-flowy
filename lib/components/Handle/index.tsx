682c9ed273da090e6b842fd1aaf7d6996ddd40e2
Huy Ta Quoc <quochuy.tl.bk@gmail.com>
2021-09-13 20:37:58 +0700

    Fixed the Handle component

--- lib/components/Handle/index.tsx
+++ lib/components/Handle/index.tsx
@@ -17,6 +17,8 @@ export interface HandleProps {
 const Handle: Component<HandleProps> = (props) => {
   props = mergeProps({ additionalEdgeProps: { type: 'standardEdge' } }, props);
   const [state, { upsertEdge, deleteElementById }] = useStoreById(props.storeId)!;
+  let previousTargetNode: Node;
+  let previousValid: boolean;
 
   const getFormingEdgeId = () => {
     return `e${props.node.id}-?`;
@@ -39,7 +41,7 @@ const Handle: Component<HandleProps> = (props) => {
 
     let targetRectangle: Rectangle;
 
-    const targetNode = Object.values(state.nodes).find(whichNode => {
+    const targetNode = Object.values(state.nodes).find((whichNode) => {
       targetRectangle = {
         x: whichNode.position.x,
         y: whichNode.position.y,
@@ -61,26 +63,47 @@ const Handle: Component<HandleProps> = (props) => {
     };
 
     if (targetNode) {
-      waypoints = connectShapes({ ...sourceRectangle, ...props.node.shapeData }, { ...targetRectangle!, ...targetNode.shapeData }, props.node.shapeType, targetNode.shapeType, undefined, cursorPosition, { preferredLayouts: [LayoutType.VERTICAL_VERTICAL] });
+      waypoints = connectShapes(
+        { ...sourceRectangle, ...props.node.shapeData },
+        { ...targetRectangle!, ...targetNode.shapeData },
+        props.node.shapeType,
+        targetNode.shapeType,
+        undefined,
+        cursorPosition,
+        { preferredLayouts: [LayoutType.VERTICAL_VERTICAL] }
+      );
       newEdge.target = targetNode.id;
       newEdge.waypoints = waypoints;
 
       const nodeValidator = state.nodeValidators[props.node.type || 'standardNode'];
 
       if (typeof nodeValidator === 'function') {
-        const { isValid } = (nodeValidator as NodeValidator)(props.node, targetNode, newEdge as Edge);
+        if (previousTargetNode !== targetNode) {
+          const { isValid } = (nodeValidator as NodeValidator)(props.node, targetNode, newEdge as Edge);
 
-        if (!isValid) newEdge.isInvalid = true;
-        else delete newEdge.isInvalid;
+          previousValid = isValid;
+
+          if (!isValid) newEdge.isInvalid = true;
+          else delete newEdge.isInvalid;
+        } else {
+          if (!previousValid) newEdge.isInvalid = true;
+          else delete newEdge.isInvalid;
+        }
       }
     } else {
-      waypoints = connectShapeToPoint({ ...sourceRectangle, ...props.node.shapeData }, props.node.shapeType, cursorPosition);
+      waypoints = connectShapeToPoint(
+        { ...sourceRectangle, ...props.node.shapeData },
+        props.node.shapeType,
+        cursorPosition
+      );
       newEdge.target = '?';
       newEdge.waypoints = waypoints;
     }
 
+    previousTargetNode = targetNode;
+
     upsertEdge(newEdge as Edge);
-  }
+  };
 
   const handleDragStop = () => {
     document.body.style.overscrollBehavior = 'unset';
@@ -88,7 +111,7 @@ const Handle: Component<HandleProps> = (props) => {
     const formingEdgeId = getFormingEdgeId();
 
     if (state.edges[formingEdgeId]) {
-      if (state.edges[formingEdgeId].target !== '?') {
+      if (state.edges[formingEdgeId].target !== '?' && !state.edges[formingEdgeId].isInvalid) {
         const newEdge = { ...state.edges[formingEdgeId], id: `e${props.node.id}-${state.edges[formingEdgeId].target}` };
 
         delete newEdge.isForming;
@@ -103,23 +126,27 @@ const Handle: Component<HandleProps> = (props) => {
     document.removeEventListener('touchmove', handleDrag);
     document.removeEventListener('mouseup', handleDragStop);
     document.removeEventListener('touchend', handleDragStop);
-  }
+  };
 
   const handleDragStart = (event: MouseEvent | TouchEvent) => {
     event.stopPropagation();
     document.body.style.overscrollBehavior = 'none';
-  
+
     document.addEventListener('mousemove', handleDrag);
     document.addEventListener('touchmove', handleDrag);
     document.addEventListener('mouseup', handleDragStop);
     document.addEventListener('touchend', handleDragStop);
-  }
+  };
 
   return (
-    <div classList={{ 'solid-flowy__handle': true, 'solid-flowy__handle--hidden': !props.shouldShowHandle }} onMouseDown={handleDragStart} onTouchStart={handleDragStart}>
+    <div
+      classList={{ 'solid-flowy__handle': true, 'solid-flowy__handle--hidden': !props.shouldShowHandle }}
+      onMouseDown={handleDragStart}
+      onTouchStart={handleDragStart}
+    >
       {props.children}
     </div>
-  )
+  );
 };
 
 export default Handle;
